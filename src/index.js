171f03431c02883bcf21cc264667ae7df30b84d3
Saurabh Daware <saurabhdaware99@gmail.com>
2020-04-15 23:22:49 +0530

    abell serve function added

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@
 const fs = require('fs');
 const path = require('path');
 
+const browserSync = require('browser-sync');
+
 const { 
   getDirectories,
   rmdirRecursiveSync,
@@ -17,8 +19,6 @@ const { generateLandingPage } = require('./landingpage-generator.js');
 function build() {
   console.log("\n>> Build started\n");
   
-  // TODO: if destinationPath is not 'dist' and has contents, ask for confirmation.
-
   // Get configured paths of destination and content
   const {destinationPath, contentPath, sourcePath} = getConfigPaths();
 
@@ -41,6 +41,43 @@ function build() {
   console.log(`\n\n>> Your blog is ready at '${destinationPath.split('/').slice(-1)}' 🐨 🎉 \n\n`);
 }
 
+function serve() {
+  const {sourcePath, contentPath, destinationPath} = getConfigPaths();
+  const bs = browserSync.create('abell-dev-server');
+
+  bs.init({
+    port: 3000,
+    server: destinationPath,
+    logLevel: 'info',
+    logPrefix: 'abell-dev-server',
+    logConnections: false,
+    notify: false,
+    reloadDelay: 1000
+  });
+
+  fs.watch(sourcePath, (eventType, fileName) => {
+    if(fileName === 'index.html') {
+      generateLandingPage();
+    } else {
+      build();
+    }
+    bs.reload();
+  })
+
+  for(let directory of getDirectories(sourcePath)) {
+    fs.watch(path.join(sourcePath, directory), (eventType, fileName) => {
+      build();
+      bs.reload();
+    })
+  }
+
+  for(let directory of getDirectories(contentPath)) {
+    fs.watch(path.join(contentPath, directory), (eventName, fileName) => {
+      generateBlog(directory);
+      bs.reload();
+    })
+  }
+}
 
 // Main
 const cliArguments = process.argv.slice(2);
@@ -50,6 +87,10 @@ switch(cliArguments[0]) {
     build();
     break;
   
+  case 'serve':
+    serve();
+    break;
+
   default:
     console.log("Unknown command 😔 .")
     break;
