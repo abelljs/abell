99f88d94dc99d9ef252d61f134058e42d5bc5aa2
Saurabh Daware <saurabhdaware99@gmail.com>
2020-04-17 15:48:23 +0530

    refactor: cseparated action.js and added commander

---
+++ src/action.js
@@ -0,0 +1,127 @@
+const fs = require('fs');
+const path = require('path');
+
+const browserSync = require('browser-sync');
+
+const { 
+  getDirectories,
+  rmdirRecursiveSync,
+  getConfigPaths,
+  forcefullySetDestination,
+  copyFolderSync,
+  boldGreen
+} = require('./helpers.js');
+
+const { generateBlog } = require('./blog-generator.js');
+const { generateLandingPage } = require('./landingpage-generator.js');
+
+
+function build({logs = 'complete'} = {logs: 'complete'}) {
+  if(logs !== 'minimum') console.log("\n>> Build started\n");
+  
+  // Get configured paths of destination and content
+  const {destinationPath, contentPath, sourcePath} = getConfigPaths();
+
+  // Refresh dist
+  rmdirRecursiveSync(destinationPath);
+  fs.mkdirSync(destinationPath);
+
+  // Copy all files from source directory and then delete [blog] directory
+  copyFolderSync(sourcePath, destinationPath)
+  rmdirRecursiveSync(path.join(destinationPath, '[blog]'));
+
+  // Generate all blogs from content directory
+  for(let blogSlug of getDirectories(contentPath)) {
+    generateBlog(blogSlug);
+
+    if(logs !== 'minimum') console.log("...Built " + blogSlug);
+  }
+
+  // generate landing page (dist/index.html)
+  generateLandingPage();
+
+  if(logs !== 'minimum') console.log("\n...Built Landing Page");
+
+
+  if(logs === 'minimum') {
+    console.log(`>> Built 🌻`)
+  } else {
+    console.log(`\n\n>> Your blog is ready at '${destinationPath.split('/').slice(-1)}' 🐨 🎉 \n\n`);
+  }
+}
+
+function serve(port = 5000) {
+  forcefullySetDestination('.debug'); // Forces to output in .cache/ directory
+
+  const {sourcePath, contentPath, destinationPath} = getConfigPaths();
+  
+  // Initial build
+  build({logs: 'minimum'});
+
+  console.log('='.repeat(process.stdout.columns));
+  console.log("\n\n💫 Abell dev server running.");
+  const bs = browserSync.create('abell-dev-server');
+
+  bs.init({
+    port,
+    server: destinationPath,
+    logLevel: 'silent',
+    logPrefix: 'abell-dev-server',
+    logConnections: false,
+    notify: false,
+    reloadDelay: 1000
+  });
+
+  console.log(`${boldGreen("Local: ")} http://localhost:${port} \n\n`);
+  console.log('='.repeat(process.stdout.columns));
+  
+  fs.watch(sourcePath, (eventType, fileName) => {
+    if(fileName === 'index.html') {
+      generateLandingPage();
+    } else {
+      build({logs: 'minimum'});
+    }
+    bs.reload();
+  })
+
+  for(let directory of getDirectories(sourcePath)) {
+    fs.watch(path.join(sourcePath, directory), (eventType, fileName) => {
+      build({logs: 'minimum'});
+      bs.reload();
+    })
+  }
+
+  
+  for(let directory of getDirectories(contentPath)) {
+    fs.watch(path.join(contentPath, directory), (eventName, fileName) => {
+      generateBlog(directory);
+      bs.reload();
+    })
+  }
+
+  function exitHandler(options, exitCode) {
+    if (options.cleanup) {
+      rmdirRecursiveSync(destinationPath);
+      console.log("\n\nBiee 🐨✌️\n");
+    }
+
+    if (options.exit) process.exit();
+  }
+
+  //do something when app is closing
+  process.on('exit', exitHandler.bind(null,{cleanup:true}));
+
+  //catches ctrl+c event
+  process.on('SIGINT', exitHandler.bind(null, {exit:true}));
+
+  // catches "kill pid" (for example: nodemon restart)
+  process.on('SIGUSR1', exitHandler.bind(null, {exit:true}));
+  process.on('SIGUSR2', exitHandler.bind(null, {exit:true}));
+
+  //catches uncaught exceptions
+  process.on('uncaughtException', exitHandler.bind(null, {exit:true}));
+
+}
+
+module.exports = {build, serve};
+
--- src/index.js
+++ src/index.js
@@ -1,144 +1,21 @@
 #!/usr/bin/env node
 
-const fs = require('fs');
-const path = require('path');
+const action = require('./action');
+const program = require('commander');
 
-const browserSync = require('browser-sync');
+program.version(require('../package.json').version);
 
-const { 
-  getDirectories,
-  rmdirRecursiveSync,
-  getConfigPaths,
-  forcefullySetDestination,
-  copyFolderSync,
-  boldGreen
-} = require('./helpers.js');
+program
+  .command('build')
+  .action(action.build);
 
-const { generateBlog } = require('./blog-generator.js');
-const { generateLandingPage } = require('./landingpage-generator.js');
-
-
-function build({logs = 'complete'} = {logs: 'complete'}) {
-  if(logs !== 'minimum') console.log("\n>> Build started\n");
-  
-  // Get configured paths of destination and content
-  const {destinationPath, contentPath, sourcePath} = getConfigPaths();
-
-  // Refresh dist
-  rmdirRecursiveSync(destinationPath);
-  fs.mkdirSync(destinationPath);
-
-  // Copy all files from source directory and then delete [blog] directory
-  copyFolderSync(sourcePath, destinationPath)
-  rmdirRecursiveSync(path.join(destinationPath, '[blog]'));
-
-  // Generate all blogs from content directory
-  for(let blogSlug of getDirectories(contentPath)) {
-    generateBlog(blogSlug);
-
-    if(logs !== 'minimum') console.log("...Built " + blogSlug);
-  }
-
-  // generate landing page (dist/index.html)
-  generateLandingPage();
-
-  if(logs !== 'minimum') console.log("\n...Built Landing Page");
-
-
-  if(logs === 'minimum') {
-    console.log(`>> Built 🌻`)
-  } else {
-    console.log(`\n\n>> Your blog is ready at '${destinationPath.split('/').slice(-1)}' 🐨 🎉 \n\n`);
-  }
-}
-
-function serve() {
-  forcefullySetDestination('.debug'); // Forces to output in .cache/ directory
-
-  const {sourcePath, contentPath, destinationPath} = getConfigPaths();
-  
-  // Initial build
-  build({logs: 'minimum'});
-
-  console.log('='.repeat(process.stdout.columns));
-  console.log("\n\n💫 Abell dev server running.");
-  const bs = browserSync.create('abell-dev-server');
-
-  bs.init({
-    port: 3000,
-    server: destinationPath,
-    logLevel: 'silent',
-    logPrefix: 'abell-dev-server',
-    logConnections: false,
-    notify: false,
-    reloadDelay: 1000
-  });
-
-  console.log(boldGreen("Local: ") + "http://localhost:3000 \n\n");
-  console.log('='.repeat(process.stdout.columns));
-  
-  fs.watch(sourcePath, (eventType, fileName) => {
-    if(fileName === 'index.html') {
-      generateLandingPage();
-    } else {
-      build({logs: 'minimum'});
-    }
-    bs.reload();
+program
+  .command('serve')
+  .option('--port [port]', 'Serve on different port')
+  .action((command) => {
+    action.serve(command.port);
   })
 
-  for(let directory of getDirectories(sourcePath)) {
-    fs.watch(path.join(sourcePath, directory), (eventType, fileName) => {
-      build({logs: 'minimum'});
-      bs.reload();
-    })
-  }
+program.parse(process.argv);
 
-  
-  for(let directory of getDirectories(contentPath)) {
-    fs.watch(path.join(contentPath, directory), (eventName, fileName) => {
-      generateBlog(directory);
-      bs.reload();
-    })
-  }
-
-  function exitHandler(options, exitCode) {
-    if (options.cleanup) {
-      rmdirRecursiveSync(destinationPath);
-      console.log("\n\nBiee 🐨✌️\n");
-    }
-
-    if (options.exit) process.exit();
-  }
-
-  //do something when app is closing
-  process.on('exit', exitHandler.bind(null,{cleanup:true}));
-
-  //catches ctrl+c event
-  process.on('SIGINT', exitHandler.bind(null, {exit:true}));
-
-  // catches "kill pid" (for example: nodemon restart)
-  process.on('SIGUSR1', exitHandler.bind(null, {exit:true}));
-  process.on('SIGUSR2', exitHandler.bind(null, {exit:true}));
-
-  //catches uncaught exceptions
-  process.on('uncaughtException', exitHandler.bind(null, {exit:true}));
-
-}
-
-// Main
-const cliArguments = process.argv.slice(2);
-
-switch(cliArguments[0]) {
-  case 'build':
-    build();
-    break;
-  
-  case 'serve':
-    serve();
-    break;
-
-  default:
-    console.log("Unknown command 😔 .")
-    break;
-}
-
+module.exports = action;
