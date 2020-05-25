5cd3bb7056ba487533febc630e8ff47fcd3254ac
Saurabh Daware <saurabhdaware99@gmail.com>
2020-05-25 14:41:26 +0530

    displaying warning in yellow instead of red

--- src/helpers.js
+++ src/helpers.js
@@ -91,7 +91,7 @@ function getAbellConfigs() {
       );
     }
   } catch (err) {
-    console.log(boldRed('>> ') + err.message);
+    console.log(yellow('>> ') + err.message);
     abellConfig = defaultConfigs;
   }
 
@@ -185,6 +185,7 @@ const boldRed = (message) =>
 const boldGreen = (message) =>
   `\u001b[1m\u001b[32m${message}\u001b[39m\u001b[22m`;
 const grey = (message) => `\u001b[90m${message}\u001b[39m`;
+const yellow = (message) => `\u001b[1m\u001b[33m${message}\u001b[39m\u001b[22m`;
 
 module.exports = {
   getAbellFiles,
@@ -197,5 +198,6 @@ module.exports = {
   execRegexOnAll,
   boldGreen,
   boldRed,
-  grey
+  grey,
+  yellow
 };
