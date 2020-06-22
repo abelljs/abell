7bade62705900f78b8ff748eb97a67097050fe46
Saurabh Daware <saurabhdaware99@gmail.com>
2020-06-22 17:20:16 +0530

    paths fix in main.js

--- src/main.js
+++ src/main.js
@@ -2,13 +2,14 @@
  * It is very unlikely that you would want to change anything in this file
  * since it only deals with exporting right functions to users.
  *
- * Follow the required paths to reach the right file
+ * When you run `abell serve` and `abell build`, `./cli.js` is the file
+ * that is executed so you can follow code from cli.js for basic logic.
  */
 
 const {
   generateContentFile,
   generateHTMLFile
-} = require('./utils/content-generator.js');
+} = require('./utils/build-utils.js');
 
 const build = require('./commands/build.js');
 const serve = require('./commands/serve.js');
