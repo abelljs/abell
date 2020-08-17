3e37ff1e196e296a63084c7738e23718cf23b7b4
Harshit Pant <pantharshit00@gmail.com>
2020-08-18 00:53:08 +0530

    test: add tests for retries

--- tests/abell-dev-server.spec.js
+++ tests/abell-dev-server.spec.js
@@ -3,6 +3,8 @@ const expect = require('chai').expect;
 const fetch = require('node-fetch');
 const path = require('path');
 const cheerio = require('cheerio');
+const net = require('net');
+const sinon = require('sinon');
 
 const SERVER_DEFAULT_PORT = 5000;
 
@@ -19,7 +21,7 @@ describe('Abell Dev Server', () => {
     process.chdir(__dirname);
   });
 
-  it('can start and responds on default port ', async () => {
+  it('starts and responds on default port ', async () => {
     serverInstance = await devServer.create({
       path: path.join(process.cwd())
     });
@@ -33,5 +35,38 @@ describe('Abell Dev Server', () => {
     const parsed = cheerio.load(textPage);
     expect(parsed('h1').text()).to.be.eq('Hello World');
     expect(parsed('script').length).to.be.eq(1);
+  });
+
+  it('starts on the next port if default port is occupied', async () => {
+    const dummyServer = net.createServer();
+    dummyServer.listen(SERVER_DEFAULT_PORT);
+
+    serverInstance = await devServer.create({
+      path: path.join(process.cwd())
+    });
+
+    const nextPort = SERVER_DEFAULT_PORT + 1;
+    expect(serverInstance.httpServer.address().port).to.be.eq(nextPort);
+    expect(serverInstance.wss.address().port).to.be.eq(nextPort);
+
+    const res = await fetch(`http://localhost:${nextPort}`);
+    expect(res.status).to.be.eq(200);
+    dummyServer.close();
+  });
+
+  it('exits finding ports after 3 retries', async () => {
+    const serverInstances = [];
+    for (let i = 0; i <= 3; i++) {
+      const dummyServer = net.createServer();
+      dummyServer.listen(SERVER_DEFAULT_PORT + i);
+      serverInstances.push(dummyServer);
+    }
+    sinon.stub(process, 'exit');
+
+    serverInstance = await devServer.create({
+      path: path.join(process.cwd())
+    });
+    expect(process.exit.calledWith(1)).to.be.true;
+    serverInstances.forEach((ins) => ins.close());
   });
 });
