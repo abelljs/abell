f2bfffe8b7a32b113398ed21b9d1026d98fc0855
saurabhdaware <saurabhdaware99@gmail.com>
2020-10-27 12:33:51 +0530

    init tests for main example

--- examples/main/main.spec.js
+++ examples/main/main.spec.js
@@ -1,279 +1,253 @@
 const path = require('path');
 
-const { expect } = require('chai');
 const {
   preTestSetup,
   getSelector
-} = require('../../tests/test-utils/test-helpers.js');
+} = require('../../tests/test-utils/helpers.js');
 
 describe('examples/main', () => {
-  before(async () => {
+  beforeAll(async () => {
     await preTestSetup('main');
   });
 
-  describe('index.html', () => {
-    let $;
-    before(() => {
-      $ = getSelector(path.join(__dirname, 'dist', 'index.html'));
+  describe('dist/index.html', () => {
+    let indexSelector;
+    const testComponents = [
+      {
+        it: 'should render basic maths and Abell Variables',
+        selector: '[data-test="basic-test"]'
+      },
+      {
+        it: 'should render contentArray loop',
+        selector: '[data-test="contentarray-container"]'
+      }
+    ];
+
+    beforeAll(() => {
+      indexSelector = getSelector(path.join(__dirname, 'dist', 'index.html'));
     });
 
-    it('should render executed JavaScript', () => {
-      expect($('[data-test="js-in-abell-test"]').html()).to.equal(String(11));
-    });
-
-    it('should render required text from JSON', () => {
-      expect($('[data-test="include-from-json-test"]').html()).to.equal(
-        'hi I am from JSON'
-      );
-    });
-
-    it('should render value from abell.config.js globalMeta', () => {
-      expect($('[data-test="globalmeta-test"]').html()).to.equal(
-        'Abell standard example'
-      );
-    });
-
-    it('should render nothing since top level value of $root is blank', () => {
-      expect($('[data-test="root-test"]').html()).to.equal('');
-    });
-
-    it('should render nothing since top level value of $path is blank', () => {
-      expect($('[data-test="path-test"]').html()).to.equal('');
-    });
-
-    it('should render all the article meta info into container', () => {
-      const expectedTitles = [
-        `sub-blog (new-blog${path.sep}sub-blog)`,
-        'new-blog (new-blog)',
-        'My First Blog (my-first-blog)',
-        'Another blog (another-blog)'
-      ];
-
-      $('[data-test="contentarray-container"] > div').each(function (
-        index,
-        element
-      ) {
-        expect($(this).children('span.data-title').html()).to.equal(
-          expectedTitles[index]
-        );
+    for (const testComponent of testComponents) {
+      it(testComponent.it, () => {
+        expect(indexSelector(testComponent.selector).html()).toMatchSnapshot();
       });
-    });
+    }
   });
 
-  // another-blog example
-  describe('another-blog/example.html', () => {
-    let $;
-    before(() => {
-      $ = getSelector(
-        path.join(__dirname, 'dist', 'another-blog', 'example.html')
-      );
-    });
+  // // another-blog example
+  // describe('another-blog/example.html', () => {
+  //   let $;
+  //   before(() => {
+  //     $ = getSelector(
+  //       path.join(__dirname, 'dist', 'another-blog', 'example.html')
+  //     );
+  //   });
 
-    it('should render body text', () => {
-      expect($('body').html().trim()).to.equal('another-blog');
-    });
-  });
+  //   it('should render body text', () => {
+  //     expect($('body').html().trim()).to.equal('another-blog');
+  //   });
+  // });
 
-  // another-blog index
-  describe('another-blog/index.html', () => {
-    let $;
-    before(() => {
-      $ = getSelector(
-        path.join(__dirname, 'dist', 'another-blog', 'index.html')
-      );
-    });
+  // // another-blog index
+  // describe('another-blog/index.html', () => {
+  //   let $;
+  //   before(() => {
+  //     $ = getSelector(
+  //       path.join(__dirname, 'dist', 'another-blog', 'index.html')
+  //     );
+  //   });
 
-    it('should render header text', () => {
-      expect($('[id="another-blog"]').html()).to.equal('Another blog');
-    });
+  //   it('should render header text', () => {
+  //     expect($('[id="another-blog"]').html()).to.equal('Another blog');
+  //   });
 
-    it('should render all blogs into blog container', () => {
-      const blogs = ['..', 'another-blog', 'another-blog'];
+  //   it('should render all blogs into blog container', () => {
+  //     const blogs = ['..', 'another-blog', 'another-blog'];
 
-      $('#blog-container span').each(function (index, element) {
-        expect($(this).html()).to.equal(blogs[index]);
-      });
-    });
+  //     $('#blog-container span').each(function (index, element) {
+  //       expect($(this).html()).to.equal(blogs[index]);
+  //     });
+  //   });
 
-    it('should render first para text', () => {
-      const dateToCheck = new Date();
-      dateToCheck.setFullYear(2020, 4, 9);
-      dateToCheck.setHours(0);
-      dateToCheck.setMinutes(0);
-      dateToCheck.setSeconds(0);
-      expect($('body main section p').first().html()).to.equal(
-        dateToCheck.toString()
-      );
-    });
+  //   it('should render first para text', () => {
+  //     const dateToCheck = new Date();
+  //     dateToCheck.setFullYear(2020, 4, 9);
+  //     dateToCheck.setHours(0);
+  //     dateToCheck.setMinutes(0);
+  //     dateToCheck.setSeconds(0);
+  //     expect($('body main section p').first().html()).to.equal(
+  //       dateToCheck.toString()
+  //     );
+  //   });
 
-    it('should render last para text', () => {
-      expect($('body main section p').last().html()).to.equal(
-        'Amazing blog right'
-      );
-    });
-  });
+  //   it('should render last para text', () => {
+  //     expect($('body main section p').last().html()).to.equal(
+  //       'Amazing blog right'
+  //     );
+  //   });
+  // });
 
-  // deep
-  describe('deep/index.html', () => {
-    let $;
-    before(() => {
-      $ = getSelector(path.join(__dirname, 'dist', 'deep', 'index.html'));
-    });
+  // // deep
+  // describe('deep/index.html', () => {
+  //   let $;
+  //   before(() => {
+  //     $ = getSelector(path.join(__dirname, 'dist', 'deep', 'index.html'));
+  //   });
 
-    it('should render html body text', () => {
-      expect($('body').html()).to.equal('ok ..');
-    });
-  });
+  //   it('should render html body text', () => {
+  //     expect($('body').html()).to.equal('ok ..');
+  //   });
+  // });
 
-  // more deep
-  describe('deep/moredeep/index.html', () => {
-    let $;
-    before(() => {
-      $ = getSelector(
-        path.join(__dirname, 'dist', 'deep', 'moredeep', 'index.html')
-      );
-    });
+  // // more deep
+  // describe('deep/moredeep/index.html', () => {
+  //   let $;
+  //   before(() => {
+  //     $ = getSelector(
+  //       path.join(__dirname, 'dist', 'deep', 'moredeep', 'index.html')
+  //     );
+  //   });
 
-    it('should render html body text', () => {
-      expect($('body').html()).to.equal(`..${path.sep}..`);
-    });
-  });
+  //   it('should render html body text', () => {
+  //     expect($('body').html()).to.equal(`..${path.sep}..`);
+  //   });
+  // });
 
-  // my first blog example
-  describe('my-first-blog/example.html', () => {
-    let $;
-    before(() => {
-      $ = getSelector(
-        path.join(__dirname, 'dist', 'my-first-blog', 'example.html')
-      );
-    });
+  // // my first blog example
+  // describe('my-first-blog/example.html', () => {
+  //   let $;
+  //   before(() => {
+  //     $ = getSelector(
+  //       path.join(__dirname, 'dist', 'my-first-blog', 'example.html')
+  //     );
+  //   });
 
-    it('should render html body text', () => {
-      expect($('body').text().trim()).to.equal('my-first-blog');
-    });
-  });
+  //   it('should render html body text', () => {
+  //     expect($('body').text().trim()).to.equal('my-first-blog');
+  //   });
+  // });
 
-  // my first blog index
-  describe('my-first-blog/index.html', () => {
-    let $;
-    before(() => {
-      $ = getSelector(
-        path.join(__dirname, 'dist', 'my-first-blog', 'index.html')
-      );
-    });
+  // // my first blog index
+  // describe('my-first-blog/index.html', () => {
+  //   let $;
+  //   before(() => {
+  //     $ = getSelector(
+  //       path.join(__dirname, 'dist', 'my-first-blog', 'index.html')
+  //     );
+  //   });
 
-    it('should render section header text', () => {
-      expect($('section #test').text()).to.equal('test');
-    });
+  //   it('should render section header text', () => {
+  //     expect($('section #test').text()).to.equal('test');
+  //   });
 
-    it('should render section hyperlink', () => {
-      expect($('section p').text()).to.equal('https://makethislink.com');
-    });
+  //   it('should render section hyperlink', () => {
+  //     expect($('section p').text()).to.equal('https://makethislink.com');
+  //   });
 
-    it('should render all blogs into blog container', () => {
-      const blogs = ['..', 'my-first-blog', 'my-first-blog'];
+  //   it('should render all blogs into blog container', () => {
+  //     const blogs = ['..', 'my-first-blog', 'my-first-blog'];
 
-      $('#blog-container span').each(function (index, element) {
-        expect($(this).html()).to.equal(blogs[index]);
-      });
-    });
-  });
+  //     $('#blog-container span').each(function (index, element) {
+  //       expect($(this).html()).to.equal(blogs[index]);
+  //     });
+  //   });
+  // });
 
-  // new blog example
-  describe('new-blog/example.html', () => {
-    let $;
-    before(() => {
-      $ = getSelector(path.join(__dirname, 'dist', 'new-blog', 'example.html'));
-    });
+  // // new blog example
+  // describe('new-blog/example.html', () => {
+  //   let $;
+  //   before(() => {
+  //     $ = getSelector(path.join(__dirname, 'dist', 'new-blog', 'example.html'));
+  //   });
 
-    it('should render html body text', () => {
-      expect($('body').text().trim()).to.equal('new-blog');
-    });
-  });
+  //   it('should render html body text', () => {
+  //     expect($('body').text().trim()).to.equal('new-blog');
+  //   });
+  // });
 
-  // new blog
-  describe('new-blog/index.html', () => {
-    let $;
-    before(() => {
-      $ = getSelector(path.join(__dirname, 'dist', 'new-blog', 'index.html'));
-    });
+  // // new blog
+  // describe('new-blog/index.html', () => {
+  //   let $;
+  //   before(() => {
+  //     $ = getSelector(path.join(__dirname, 'dist', 'new-blog', 'index.html'));
+  //   });
 
-    it('should render all blogs into blog container', () => {
-      const blogs = ['..', 'new-blog', 'new-blog'];
+  //   it('should render all blogs into blog container', () => {
+  //     const blogs = ['..', 'new-blog', 'new-blog'];
 
-      $('#blog-container span').each(function (index, element) {
-        expect($(this).html()).to.equal(blogs[index]);
-      });
-    });
-    it('should render header text', () => {
-      expect($('body main section #newblog').html()).to.equal('new-blog');
-    });
-    it('should render first para text', () => {
-      expect($('body main section p').first().html()).to.equal('..');
-    });
-    it('should render global meta site name', () => {
-      expect($('body main section p').last().html()).to.equal(
-        '{{ Abell.globalMeta.siteName }}'
-      );
-    });
-  });
+  //     $('#blog-container span').each(function (index, element) {
+  //       expect($(this).html()).to.equal(blogs[index]);
+  //     });
+  //   });
+  //   it('should render header text', () => {
+  //     expect($('body main section #newblog').html()).to.equal('new-blog');
+  //   });
+  //   it('should render first para text', () => {
+  //     expect($('body main section p').first().html()).to.equal('..');
+  //   });
+  //   it('should render global meta site name', () => {
+  //     expect($('body main section p').last().html()).to.equal(
+  //       '{{ Abell.globalMeta.siteName }}'
+  //     );
+  //   });
+  // });
 
-  // new blog/sub blog/example
-  describe('new-blog/sub-blog/example.html', () => {
-    let $;
-    before(() => {
-      $ = getSelector(
-        path.join(__dirname, 'dist', 'new-blog', 'sub-blog', 'example.html')
-      );
-    });
+  // // new blog/sub blog/example
+  // describe('new-blog/sub-blog/example.html', () => {
+  //   let $;
+  //   before(() => {
+  //     $ = getSelector(
+  //       path.join(__dirname, 'dist', 'new-blog', 'sub-blog', 'example.html')
+  //     );
+  //   });
 
-    it('should render html body text', () => {
-      expect($('body').text().trim()).to.equal(`new-blog${path.sep}sub-blog`);
-    });
-  });
+  //   it('should render html body text', () => {
+  //     expect($('body').text().trim()).to.equal(`new-blog${path.sep}sub-blog`);
+  //   });
+  // });
 
-  describe('new-blog/sub-blog/index.html', () => {
-    let $;
-    before(() => {
-      $ = getSelector(
-        path.join(__dirname, 'dist', 'new-blog', 'sub-blog', 'index.html')
-      );
-    });
+  // describe('new-blog/sub-blog/index.html', () => {
+  //   let $;
+  //   before(() => {
+  //     $ = getSelector(
+  //       path.join(__dirname, 'dist', 'new-blog', 'sub-blog', 'index.html')
+  //     );
+  //   });
 
-    it('should render all blogs into blog container', () => {
-      const blogs = [
-        `..${path.sep}..`,
-        `new-blog${path.sep}sub-blog`,
-        'sub-blog'
-      ];
+  //   it('should render all blogs into blog container', () => {
+  //     const blogs = [
+  //       `..${path.sep}..`,
+  //       `new-blog${path.sep}sub-blog`,
+  //       'sub-blog'
+  //     ];
 
-      $('#blog-container span').each(function (index, element) {
-        expect($(this).html()).to.equal(blogs[index]);
-      });
-    });
+  //     $('#blog-container span').each(function (index, element) {
+  //       expect($(this).html()).to.equal(blogs[index]);
+  //     });
+  //   });
 
-    it('should render header text', () => {
-      expect($('body main section #inside-the-new-blog').html()).to.equal(
-        'Inside the new blog'
-      );
-    });
+  //   it('should render header text', () => {
+  //     expect($('body main section #inside-the-new-blog').html()).to.equal(
+  //       'Inside the new blog'
+  //     );
+  //   });
 
-    it('should render first para text', () => {
-      expect($('body main section > p:nth-child(2)').text()).to.equal(
-        `..${path.sep}..`
-      );
-    });
+  //   it('should render first para text', () => {
+  //     expect($('body main section > p:nth-child(2)').text()).to.equal(
+  //       `..${path.sep}..`
+  //     );
+  //   });
 
-    it('should have relative path to index.md', () => {
-      expect($('body main section > p:nth-child(3) > a').attr('href')).to.equal(
-        `hello`
-      );
-    });
+  //   it('should have relative path to index.md', () => {
+  //     expect($('body main section > p:nth-child(3) > a').attr('href')).to.equal(
+  //       `hello`
+  //     );
+  //   });
 
-    it('should add prefix in paths for deep level folders', () => {
-      expect($('main img').attr('src')).to.equal(
-        `..${path.sep}../image/cool.png`
-      );
-    });
-  });
+  //   it('should add prefix in paths for deep level folders', () => {
+  //     expect($('main img').attr('src')).to.equal(
+  //       `..${path.sep}../image/cool.png`
+  //     );
+  //   });
+  // });
 });
--- tests/test-utils/helpers.js
+++ tests/test-utils/helpers.js
@@ -1,4 +1,37 @@
+const fs = require('fs');
+const util = require('util');
 const path = require('path');
+const exec = util.promisify(require('child_process').exec);
+
+const cheerio = require('cheerio');
+
+/**
+ * Builds the example
+ * @param {String} exampleName Name of the example to build
+ */
+async function preTestSetup(exampleName) {
+  const { stderr, stdout } = await exec(
+    `cd examples/${exampleName} && node ../../bin/abell.js build`
+  );
+  if (stderr) throw new Error(stderr);
+  if (stdout.includes('Abell Build Failed')) {
+    console.log(stdout);
+    throw new Error(
+      `>> Build Fail for examples/${exampleName}, Scroll up for logs.`
+    );
+  }
+}
+
+/**
+ * Returns jQuery Selector for given path
+ * @param {String} outPath Path of HTML file
+ * @return {Object}
+ */
+function getSelector(outPath) {
+  const htmlTemplate = fs.readFileSync(outPath, 'utf-8');
+  const $ = cheerio.load(htmlTemplate);
+  return $;
+}
 
 /**
  * Returns responsive path that remains same even in windows
@@ -8,5 +41,7 @@ const path = require('path');
 const resPath = (pathString) => pathString.replace(/\//g, path.sep);
 
 module.exports = {
-  resPath
+  resPath,
+  preTestSetup,
+  getSelector
 };
