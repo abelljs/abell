96a785d9d5a2b98389c7045584edc50c38d77c65
saurabhdaware <saurabhdaware99@gmail.com>
2022-02-06 14:49:26 +0530

    setup local env for example

--- packages/abell-examples/src/abell-vite-example/vite.config.ts
+++ packages/abell-examples/src/abell-vite-example/vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
-import { vitePluginAbell } from '../../../abell-renderer/src/vite-plugin-abell';
+import { vitePluginAbell } from '../../../abell/src/vite-plugin-abell';
 
 export default defineConfig({
   plugins: [vitePluginAbell()]
