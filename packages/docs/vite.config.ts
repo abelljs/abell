82a6c0fb1dbff07c9090e756fa3a6a244c169530
saurabhdaware <saurabhdaware99@gmail.com>
2023-08-09 23:55:10 +0530

    feat: add footer to docs

--- packages/docs/vite.config.ts
+++ packages/docs/vite.config.ts
@@ -1,9 +1,11 @@
+import path from 'path';
 import { AbellViteConfig, defineConfig } from 'abell';
 import vitePluginMdToHTML from 'vite-plugin-md-to-html';
 import mdx from '@mdx-js/rollup';
 import { vitePluginMdxToHTML } from 'vite-plugin-mdx-to-html';
 import { abellHighlighter } from './utils/abell-syntax-highlighter';
 import { isoImport } from 'vite-plugin-iso-import';
+import viteSvgToWebfont from 'vite-svg-2-webfont';
 
 export default defineConfig({
   plugins: [
@@ -17,6 +19,10 @@ export default defineConfig({
       }
     }),
     mdx(),
-    vitePluginMdxToHTML()
+    vitePluginMdxToHTML(),
+    viteSvgToWebfont({
+      context: path.resolve('assets/icons'),
+      dest: path.resolve('assets')
+    })
   ]
 }) as AbellViteConfig;
