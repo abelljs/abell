748e226a76fddfece48a18a2fd6dee3af01d419d
saurabhdaware <saurabhdaware99@gmail.com>
2023-08-19 17:48:50 +0530

    docs: add copy button functionality

--- packages/docs/client/main.ts
+++ packages/docs/client/main.ts
@@ -7,3 +7,19 @@ import { registerSyntaxHighlighter } from './registerSyntaxHighlighter.js';
 const hljs = registerSyntaxHighlighter();
 
 hljs.initHighlightingOnLoad();
+
+document
+  .querySelectorAll<HTMLButtonElement>('button[data-copydata]')
+  .forEach((copyButton) => {
+    copyButton.addEventListener('click', () => {
+      const copyText = copyButton.dataset.copydata;
+      if (copyText) {
+        navigator.clipboard.writeText(copyText).then(() => {
+          copyButton.classList.add('copied');
+          setTimeout(() => {
+            copyButton.classList.remove('copied');
+          }, 2000);
+        });
+      }
+    });
+  });
