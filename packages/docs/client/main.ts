a941a8f1e97af157b2b83494a7e85658c4e37c5e
saurabhdaware <saurabhdaware99@gmail.com>
2023-08-19 19:15:55 +0530

    feat: make docs page responsive

--- packages/docs/client/main.ts
+++ packages/docs/client/main.ts
@@ -1,7 +1,7 @@
 import '@fontsource/inter/400.css';
 // import 'virtual:vite-svg-2-webfont.css';
 import './global.css';
-import '../client/mdxUtils.scss';
+import './mdxUtils.scss';
 import { registerSyntaxHighlighter } from './registerSyntaxHighlighter.js';
 
 const hljs = registerSyntaxHighlighter();
@@ -23,3 +23,20 @@ document
       }
     });
   });
+
+const hamburgerMenu = document.querySelector<HTMLButtonElement>(
+  '.hamburger-menu'
+);
+const docsNavbar = document.querySelector<HTMLDivElement>('.docs-navbar');
+const navbarOverlay = document.querySelector<HTMLDivElement>(
+  'header + .overlay'
+);
+
+if (hamburgerMenu && docsNavbar && navbarOverlay) {
+  hamburgerMenu.addEventListener('click', () => {
+    docsNavbar.classList.toggle('show');
+  });
+  navbarOverlay.addEventListener('click', () => {
+    docsNavbar.classList.remove('show');
+  });
+}
