4f129c1eeed185edf68cbaafa0f3bc3543b89f08
saurabhdaware <saurabhdaware99@gmail.com>
2023-08-28 02:29:04 +0530

    feat(docs): add submenu to sidebar

--- packages/docs/client/docs.ts
+++ packages/docs/client/docs.ts
@@ -21,10 +21,47 @@ if (hamburgerMenu && docsNavbar && navbarOverlay) {
 
 // Make subheadings
 const subheadings = document.querySelectorAll<HTMLHeadingElement>('h2, h3');
+
+const observer = new IntersectionObserver(() => {
+  subheadings.forEach((subheading) => {
+    const anchor = document.querySelector(
+      `.submenu a[href="#${subheading.id}"]`
+    );
+
+    if (!anchor) {
+      return;
+    }
+
+    if (subheading.getBoundingClientRect().bottom <= 0) {
+      anchor.classList.add('done');
+    } else {
+      anchor.classList.remove('done');
+    }
+  });
+});
+
+const subMenuContainer = document.querySelector<HTMLUListElement>('.submenu');
+let subMenuLists = '';
 subheadings.forEach((subheading) => {
   const text = subheading.innerText;
+
   const id = text.toLowerCase().replace(/ /g, '-');
   subheading.id = id;
   subheading.innerHTML = subheading.innerHTML + `<a href="#${id}">#</a>`;
-  console.log({ text, id });
+  subMenuLists += `
+    <li>
+      <a href="#${id}">
+        <div>
+          <i class="dash">-</i>
+          <i class="icon icon-check"></i>
+        </div>
+        <span class="menu-text">${text}</span>
+      </a>
+    </li>
+  `;
+  observer.observe(subheading);
 });
+
+if (subMenuContainer) {
+  subMenuContainer.innerHTML = subMenuLists;
+}
