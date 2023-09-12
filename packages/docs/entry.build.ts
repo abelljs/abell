4e83bfd8cba8ba284284a92f73a559cd08fd52ea
saurabhdaware <saurabhdaware99@gmail.com>
2023-09-12 23:47:34 +0530

    feat: fetch sponsors with api

--- packages/docs/entry.build.ts
+++ packages/docs/entry.build.ts
@@ -1,4 +1,6 @@
 import { Route } from 'abell';
+import path from 'path';
+import { fetchBuilder, FileSystemCache } from 'node-fetch-cache';
 
 // Layouts
 import index from './index.abell';
@@ -41,11 +43,27 @@ const docsPaths = [
   // }
 ];
 
+const MINUTE = 60 * 1000;
+
+const fetchWithCache = fetchBuilder.withCache(
+  new FileSystemCache({
+    ttl: 15 * MINUTE,
+    cacheDirectory: path.resolve('./.cache')
+  })
+);
+
+const sponsorsData = await fetchWithCache(
+  'https://ghs.vercel.app/v2/sponsors/saurabhdaware'
+).then((res) => res.json());
+
+// Current and past are swapped - https://github.com/filiptronicek/gh-sponsors-api/issues/27
+const sponsors = sponsorsData.sponsors.past;
+
 export const makeRoutes = (): Route[] => {
   return [
     {
       path: '/',
-      render: () => index()
+      render: () => index({ sponsors })
     },
     {
       path: '/webcontainer',
