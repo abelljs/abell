ead7513b368764aaead4a6e46a5f0d826cb10724
Tim Smith <69870077+timscodebase@users.noreply.github.com>
2023-10-06 19:59:53 -0700

    abell-with-tailwind

---
+++ playground/abell-with-tailwind/config.js
@@ -0,0 +1,10 @@
+const config = {
+  title: "Tim's Abell Template",
+  description: 'An Abell template with Tailwind CSS',
+  keywords: ['abell', 'tailwind', 'tailwindcss'],
+  author: 'Tim Smith',
+  github: 'https://github.com/timscodebase',
+  linkedin: 'https://www.linkedin.com/in/timsmith25/',
+}
+
+export default config
